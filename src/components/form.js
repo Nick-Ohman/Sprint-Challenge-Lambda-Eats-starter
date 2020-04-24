7647756de3fb5818c83b78f9361b55db55abacbe
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-04-24 10:24:35 -0500

    innital commit, mostly set up

---
+++ src/components/form.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react";
+import {Route, Link, Switch} from "react-router-dom";
+import * as yup from "yup";
+import axios from "axios";
+
+
+
+
+
+
+function Form(){
+    
+    
+    return (
+        <>
+   
+
+</>
+    )
+}
+
+export default Form;
---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Route, Link, Switch} from "react-router-dom";
+
+
+function Home(){
+
+    return(
+        <>
+        <header>
+        <h1>Welcome to Lambda Eats</h1>
+        </header>
+        </>
+        );
+}
+
+export default Home;
